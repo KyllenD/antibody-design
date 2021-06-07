2d40a1ff8ee83ccee7825164023f429a87dfd83a
Kyllen Dilsook <68296178+KyllenD@users.noreply.github.com>
2021-06-07 13:05:40 +0200

    Update extract_chains.tcl
    More specific atom selection for glycans

--- tcls/extract_chains.tcl
+++ tcls/extract_chains.tcl
@@ -2,7 +2,7 @@ mol load pdb lowestEstr
 set a  [atomselect top "protein and chain A or protein and chain H"]
 set b  [atomselect top "protein and chain B or protein and chain L"]
 set c  [atomselect top "protein and chain C or protein and chain P"]
-set d  [atomselect top "not protein and not water and not ions"]
+set d  [atomselect top "not protein and not water and not ions and resname ANE or resname AGA or resname BGA"]
 $a writepdb proc.pdb
 $b writepdb prob.pdb
 $c writepdb proa.pdb
