2120d402fa40d4310206b30d035d3d362b0ed22d
Kyllen Dilsook <68296178+KyllenD@users.noreply.github.com>
2022-02-25 14:08:14 +0200

    Add files via upload

---
+++ vmd_rmsd.tcl
@@ -0,0 +1,19 @@
+proc print_rmsd_through_time {{mol top}} {
+	set reference [atomselect $mol "residue 0 to 10" frame 0]
+	set compare [atomselect $mol "residue 0 to 10"]
+	set num_steps [molinfo $mol get numframes]
+	set filename "rmsd.txt"
+	set fileID [open $filename "w"]
+	for {set frame 0} {$frame < $num_steps} {incr frame} {
+		$compare frame $frame
+		set trans_mat [measure fit $compare $reference]
+		$compare move $trans_mat
+		set rmsd [measure rmsd $compare $reference]
+		puts $fileID "$frame $rmsd"
+		
+	}
+	close $fileID
+}
+
+print_rmsd_through_time
+quit
