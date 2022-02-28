5b2ee74a1aa07159d054e93c4dcf73d55847384b
Kyllen Dilsook <68296178+KyllenD@users.noreply.github.com>
2022-02-28 11:23:38 +0200

    Update vmd_rmsd.tcl

--- vmd_rmsd.tcl
+++ vmd_rmsd.tcl
@@ -3,8 +3,8 @@ proc print_rmsd_through_time {{mol top}} {
 	set end [lindex [$sel get residue] 0]
 	set start [expr {$end - 10}] 
 	set ligand [puts "residue $start to $end"]
-	set reference [atomselect $mol \"$ligand\" frame 0]
-	set compare [atomselect $mol \"$ligand\"]
+	set reference [atomselect $mol "\"$ligand\"" frame 0]
+	set compare [atomselect $mol "\"$ligand\""]
 	set num_steps [molinfo $mol get numframes]
 	set filename "rmsd.txt"
 	set fileID [open $filename "w"]
