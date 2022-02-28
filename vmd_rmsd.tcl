ea0b5ebb6821503631669c54b64d8abe0b8b6c8b
Kyllen Dilsook <68296178+KyllenD@users.noreply.github.com>
2022-02-28 11:28:06 +0200

    Update vmd_rmsd.tcl

--- vmd_rmsd.tcl
+++ vmd_rmsd.tcl
@@ -1,10 +1,6 @@
 proc print_rmsd_through_time {{mol top}} {
-	set sel [atomselect top "resname AGALN"]
-	set end [lindex [$sel get residue] 0]
-	set start [expr {$end - 10}] 
-	set ligand [puts "residue $start to $end"]
-	set reference [atomselect $mol "\"$ligand\"" frame 0]
-	set compare [atomselect $mol "\"$ligand\""]
+	set reference [atomselect $mol "ligand" frame 0]
+	set compare [atomselect $mol "ligand"]
 	set num_steps [molinfo $mol get numframes]
 	set filename "rmsd.txt"
 	set fileID [open $filename "w"]
