18d0839490998d3ea95512bc698135de017c4db6
Kyllen Dilsook <68296178+KyllenD@users.noreply.github.com>
2022-03-05 16:55:55 +0200

    Add files via upload
    creates files containing vmd selection string for ligand

---
+++ lig_sel.tcl
@@ -0,0 +1,14 @@
+set sel [atomselect top "resname AGALN"]
+set end [lindex [$sel get residue] 0]
+set start [expr {$end - 10}] 
+set fileID [open "vmd_ligand_selection.txt" "w"]
+puts $fileID "residue $start to $end"
+
+set mda_end [expr {$end + 1}]
+set mda_start [expr {$start + 1}]
+set file2ID [open "MDA_ligand_selection.txt" "w"]
+puts $file2ID "resnum $mda_start-$mda_end"
+
+#set file3ID [open "parmed_ligand_selection.txt" "w"]
+#puts $file3ID $mda_start-$mda_end
+quit
