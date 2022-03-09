01ec4a860119192f0c329dc2c647bf16697a036c
Kyllen Dilsook <68296178+KyllenD@users.noreply.github.com>
2022-03-09 19:21:22 +0200

    Update lig_sel.tcl

--- lig_sel.tcl
+++ lig_sel.tcl
@@ -4,11 +4,11 @@ set start [expr {$end - 10}]
 set fileID [open "vmd_ligand_selection.txt" "w"]
 puts $fileID "residue $start to $end"
 
-set mda_end [expr {$end + 1}]
-set mda_start [expr {$start + 1}]
+#set mda_end [expr {$end + 1}]
+#set mda_start [expr {$start + 1}]
 #set file2ID [open "MDA_ligand_selection.txt" "w"]
 #puts $file2ID "resnum $mda_start-$mda_end"
 
-set file3ID [open "parmed_ligand_selection.txt" "w"]
-puts $file3ID $mda_start-$mda_end
+#set file3ID [open "parmed_ligand_selection.txt" "w"]
+#puts $file3ID $mda_start-$mda_end
 quit
